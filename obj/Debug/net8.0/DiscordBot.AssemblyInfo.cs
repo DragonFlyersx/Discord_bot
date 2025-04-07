b9f077f91d8ed87fb1db1a2aee47106d163d838a
Mathias Niklas Larsen <143803020+DragonFlyersx@users.noreply.github.com>
2025-04-07 20:43:11 +0200

    Ipdate

--- obj/Debug/net8.0/DiscordBot.AssemblyInfo.cs
+++ obj/Debug/net8.0/DiscordBot.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DiscordBot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca3d7265ceef6f9a8abc813a2d38da683a94d68f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ccf29710fce534288631a9604561ed03a5b0320b")]
 [assembly: System.Reflection.AssemblyProductAttribute("DiscordBot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DiscordBot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
